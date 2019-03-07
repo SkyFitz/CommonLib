d5b671b29219c8b7f23a6e925d311c7c18b00943
fitz <ljo_hz@aliyun.com>
2019-03-07 10:27:10 +0800

    1.0.1

--- app/src/main/java/com/fitz/commonlib/MainActivity.java
+++ app/src/main/java/com/fitz/commonlib/MainActivity.java
@@ -3,11 +3,16 @@ package com.fitz.commonlib;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.android.library.base.BaseAppCompatActivity;
+import com.android.library.util.ToastUtils;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ToastUtils.LongToast(getApplicationContext(), "哈哈");
     }
 }
